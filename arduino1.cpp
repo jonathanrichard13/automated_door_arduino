7f256c859bfee812f2148ae5d3b729666a8066a1
jonathanrichard13 <13519128@std.stei.itb.ac.id>
2022-04-23 22:26:07 +0700

    Update arduino1.cpp

--- arduino1.cpp
+++ arduino1.cpp
@@ -1,62 +1,146 @@
-// C++ code
-//
+#include<LiquidCrystal.h>
+#include <Keypad.h>
+#include <Servo.h>
 #include <Wire.h>
 
-#define EMPTY 0
-#define NONEMPTY 1
-#define DOOROPENFROMOUTSIDE 2
-#define DOOROPENFROMINSIDE 3
+
+#define UNACTIVE 0
+#define ACTIVE 1
+#define UNLOCKED 2
+#define TIMEOUT 3
+
+// SERVO
+Servo servo;
+
+// LCD
+LiquidCrystal lcd(12, 11, A0, A1, A2, A3);
+
+const byte numRows= 4;
+const byte numCols= 4;
+
+// KEYPAD
+char keymap[numRows][numCols]= 
+{
+    {'1', '2', '3', 'A'}, 
+    {'4', '5', '6', 'B'}, 
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}
+};
+
+//Code that shows the the keypad connections to the arduino terminals
+byte rowPins[numRows] = {9,8,7,6}; //Rows 0 to 3
+byte colPins[numCols]= {5,4,3,2}; //Columns 0 to 3
+
+//initializes an instance of the Keypad class
+Keypad myKeypad= Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+
+int state = UNACTIVE;
+unsigned long start;
+unsigned long wrongPasswordStart = 0;
+String inputCode = ""; 
+char keypressed = '#';
+String passcode = "1234";
 
 const int slaveAddress = 10;
-int state = EMPTY;
-int outsideSensor = 11;
-int insideSensor = 10;
-int lightBulb = 8;
-
 
 void setup()
 {
-  	pinMode(lightBulb, OUTPUT);
     Serial.begin(9600);
-    Wire.begin(slaveAddress);
-    Wire.onReceive(receiveEvent);
-}
-
-void receiveEvent(int something)
-{
-	Serial.println(Wire.read());
+    lcd.begin(16, 2); 
+    servo.attach(10, 500, 2500);
+    Wire.begin();
 }
 
 void loop()
 {
-  	Serial.println(digitalRead(outsideSensor));
+    keypressed = myKeypad.getKey();
     switch (state)
     {
-    case EMPTY:
-      Serial.println("Here");
-        // Turn off light bulb 
-        digitalWrite(lightBulb, HIGH);
-        break;
+        case UNACTIVE:
+            if (keypressed == 'A'){
+                start = millis();
+                state = ACTIVE;
+            } else {
+            	lcd.clear();
+            }
+            break;
+        case ACTIVE:
+      		if (millis() - wrongPasswordStart < 500){
+      			// Do nothing
+            }
+            else if (inputCode.length() == 4){
+                checkPassword();
+            } else {
+                promptStart(15);
+            }
+            break;
+        case UNLOCKED:
+                Wire.beginTransmission(slaveAddress);
+                Wire.write(true);
+                Wire.endTransmission();
+                delay(2000);
+                inputCode = "";
+                state = UNACTIVE;
+            break;
+        case TIMEOUT:
+      		timeout();
+            break;
+        default:
+            break;
+    }
 
-    case DOOROPENFROMOUTSIDE:
-        digitalWrite(lightBulb, HIGH);
-        break;
+}
 
-    case DOOROPENFROMINSIDE:
-        digitalWrite(lightBulb, HIGH);
 
-        break;
-    
-    default:
-        break;
+
+void promptStart(int countdown){
+    lcd.setCursor(0, 0);
+    lcd.print("Password: ");
+    if (millis() - start < countdown*1000){
+        lcd.setCursor(0, 1);
+        lcd.print("Time left: " + (String) (countdown - (millis() - start)/1000));
+        keypressed = myKeypad.getKey();
+        if (isDigit(keypressed)){
+            inputCode += keypressed;
+        }
+
+        lcd.setCursor(10, 0);
+        lcd.print(inputCode);
+    } else {
+        state = TIMEOUT;
+      	start = millis();
     }
 }
 
-//   position = 0;
-//   for (position = 1; position <= 179; position += 1) {
-//     servo_9.write(position);
-//     delay(20); // Wait for 20 millisecond(s)
-//   }
-//   for (position = 179; position >= 1; position -= 1) {
-//     servo_9.write(position);
-//   }
+void checkPassword(){
+    if (inputCode == passcode){
+        lcd.setCursor(0, 0);
+        lcd.print("UNLOCKED        ");
+        clearLCD(1);
+        state = UNLOCKED;
+    } else {
+        inputCode = "";
+        lcd.setCursor(0, 0);
+        lcd.print("INCORRECT       ");
+        lcd.setCursor(0, 1);
+        lcd.print("PASSWORD        ");
+        wrongPasswordStart = millis();
+    }
+}
+
+void timeout(){
+    if (millis() - start < 10000){
+        lcd.setCursor(0, 0);
+        lcd.print("TIMEOUT         ");
+        lcd.setCursor(0, 1);
+        lcd.print("Waiting time: " + ((String) (10 - (millis() - start)/1000)) + " ");
+    } else {
+        state = UNACTIVE; 
+    }
+}
+
+void clearLCD(int row){
+    lcd.setCursor(0, row);
+    lcd.print("                "); 
+}
+
